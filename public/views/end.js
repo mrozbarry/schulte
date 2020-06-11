6cc56266619af1949b3d4bc6b299afa1e9f3ebf4
Alex Barry <alex.barry@gmail.com>
2020-06-11 21:03:58 +0000

    Display end game statistics
    Co-authored-by: Alex Barry <alex.barry@gmail.com>
    Co-authored-by: Dan Soppelsa <dsoppelsa@gmail.com>
    Co-authored-by: Alisa Vynohradova <alice.7414122013@gmail.com>
    Co-authored-by: Nick Down <nick@nickdown.com>
    Co-authored-by: Joel Repta <joel.repta@gmail.com>

--- public/views/end.js
+++ public/views/end.js
@@ -2,9 +2,37 @@
 import { h } from 'https://unpkg.com/hyperapp';
 import * as actions from '../actions.js';
 
+const totalTime = (state) => {
+  return state.numbers.reduce((total, number) => {
+    return total + number.time;
+  }, 0);
+}
+
+const totalError = (state) => {
+  return state.numbers.reduce((total, number) => {
+    return total + number.errors;
+  }, 0);
+}
+
 export const end = (state) => h('div', {}, 
   [
-    'Game Over',
+    h('h1', {}, 'Game Over'),
+    h(
+        'div', 
+        {}, 
+        [`Total time: ${totalTime(state)/1000}`],
+    ),
+    h('h1', {}, `Total errors: ${totalErrors(state)}`),
+    state.numbers.map(number => {
+      return h(
+        'div', 
+        {}, 
+        [`For number ${number.number} your time was ${number.time / 1000} seconds`])
+    }),
+
+    
+    h('hr'),
+
     h('button', {
       onclick: actions.resetGame,
     }, 'Play Again'),
